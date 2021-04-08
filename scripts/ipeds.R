4d36fcfad3690938c7208a6d4c2323e3ae3f5ca2
Will Doyle <wdoyle42@gmail.com>
2021-04-08 11:36:29 -0500

    1st version of dataset complete

--- scripts/ipeds.R
+++ scripts/ipeds.R
@@ -41,7 +41,7 @@ years<-2019
 ## IPEDS institutional characteristics (using HD files)
 
 filenames<-paste0('HD',2019,'.zip')
-var <- c('unitid','instnm','city','stabbr','control','sector','carnegie', 'ccipug','c15basic')
+var <- c('unitid','instnm','city','stabbr','control','sector','carnegie', 'ccipug','c15basic','obereg')
 hd_df <- build.dataset.ipeds(filenames=filenames, datadir = rddir, vars = var,years=years)
 
 ## IPEDS enrollments (using EFIA files)
@@ -73,94 +73,28 @@ pattern <- '*\\_df\\b'; byvar <- c('unitid', 'year')
 inst <- merge.ipeds(pattern = pattern, byvar = byvar)
 
 ## =============================================================================
-## Structure
+## Add full state names
 ## =============================================================================
-
-## required library
-library(tidyverse)
-
-## merge in statadd full state name for state abbreviation
-sl <- read.csv(paste0(mddir, 'statename.csv'))
-inst <- merge(inst, sl, by = 'stabbr', all.x = TRUE)
-
-
 
 
 ## =============================================================================
 ## Some misc cleanup
 ## =============================================================================
 
-##Drop if no undergrads
-
-inst<-inst[inst$fteug>0,]
-
-## drop cc of the airforce
-inst <- inst[inst$unitid != 100636, ]
-
-## Drop military academies
-
-mil.ids<-c(128328,
-           130624,
-           197027,
-           197036,
-           164155,
-           164155
-)
-
-inst<-filter(inst,!(unitid%in%mil.ids))
-
-
-## REPORT CATEGORIES -----------------------------------------------------------
-##
-## 1 = Public 2 year (associate's dominant)
-## 2 = Public Non-Doctoral (all other 4-years)
-## 3= Public Doctoral (includes extensive and intensive) 4-year
-## 4 = Private Non-Doctoral (as above)
-## 5 = Private Doctoral (as above)
-## -----------------------------------------------------------------------------
-
-## Initialize grouping
-inst$group <- NA
-
-## All public four years
-inst$group[inst$sector==1] <- 2
-
-#Reassign public doctoral
-inst$group[inst$group==2 & inst$carnegie %in% c(15:16)]<-3
-
-## All private four years
-inst$group[inst$sector==2]<-4
-
-## Reassign private doctoral              
-
-inst$group[inst$group==4 & inst$carnegie %in% c(15:16)]<-5
-
-## Public 2 years
-
-inst$group[inst$sector==4]<-1
-
-## Reassign any asscoiate dominant to 2 years from 4 years
-
-inst$group[inst$group==2 & inst$ccipug==2]<-1
-inst$group[inst$group==2& inst$cc15basic==14]<-1
-
-## Fix UM augusta
-
-inst$group[inst$unitid==161217]<-2
-
-## =============================================================================
-## CLEAN
-## =============================================================================
-
-## drop if group is NA since it cannot be used
-inst <- inst[!is.na(inst$group),]
-
-## resort dataframe; order variables; reset rownames
-inst <- inst[order(inst$unitid, inst$year, inst$group,inst$faminccat),]
-inst<-inst[c("unitid","instnm","stabbr","statename","year","group","fteug","faminccat","netprice")]
+inst<-inst%>%
+  filter(fteug>0,   ##Drop if no undergrads
+         sector!=0,  ## drop admin units 
+         obereg!=0, ## drop military academies
+         unitid != 100636 ) ## drop cc of the airforce
+  
 
 ##Drop rownames
 rownames(inst) <- NULL
+
+## Select just publics
+
+inst<-inst%>%
+  filter(control==1)
 
 ## =============================================================================
 ## OUTPUT FINAL DATASET AS .CSV
